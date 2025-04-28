7cc213ca456c92280596033c5880a9b55bc01741
Nishrit Baral <nishritbaral@gmail.com>
2025-04-28 23:36:58 +0545

    added icon using mui

--- app/components/list.tsx
+++ app/components/list.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import Image from "next/image";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
-type card = {
+type Card = {
   id: number;
   title: string;
   price: number;
@@ -14,20 +15,24 @@ type card = {
   };
 };
 
-const List = async () => {
+const getData = async () => {
   const data = await fetch("https://fakestoreapi.com/products", {
     next: { revalidate: 600 },
   });
+  return data.json();
+};
+
+const List = async () => {
   // console.log(data);
-  const posts: card[] = await data.json();
+  const posts: Card[] = await getData();
   // console.log(posts);
 
   return (
     <div className="flex flex-row items-center justify-center p-2 m-2 gap-8 max-w-lvw flex-wrap ">
-      {posts.map((item: card) => (
+      {posts.map((item: Card) => (
         <div
           key={item.id}
-          className="w-58 h-96 border-1 border-white rounded-lg  opacity-80 hover:opacity-100 hover:shadow-lg hover:shadow-white hover:scale-105 transition-all ease-in-out duration-250 "
+          className=" flex flex-col w-58 h-96 border-1 border-white rounded-lg  opacity-80 hover:opacity-100 hover:shadow-lg hover:shadow-white hover:scale-105 transition-all ease-in-out duration-200"
         >
           <Image
             src={item.image}
@@ -36,9 +41,13 @@ const List = async () => {
             height={500}
             className="w-full h-52 rounded-lg "
           />
-          <div className="flex flex-col p-4">
+          <div className="flex flex-col flex-grow p-4 justify-between">
             <h2>{item.id}</h2>
             <p>{item.title}</p>
+
+            <div className="flex justify-end items-end ">
+              <ExpandMoreIcon className=" w-32 h-32 border-2 border-white rounded-full bg-black text-white  hover:bg-white hover:text-black hover:scale-110 transition-all duration-300 ease-in-out" />
+            </div>
           </div>
         </div>
       ))}
