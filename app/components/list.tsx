b61ad53aeefe68b6f7c7fb72a4ed222c5bb6b97a
Nishrit Baral <nishritbaral@gmail.com>
2025-04-28 21:26:54 +0545

    added hover effects on the cards

--- app/components/list.tsx
+++ app/components/list.tsx
@@ -19,22 +19,21 @@ const List = async () => {
   });
   // console.log(data);
   const posts: card[] = await data.json();
-  console.log(posts);
+  // console.log(posts);
 
   return (
     <div className="flex flex-row items-center justify-center gap-8 max-w-lvw flex-wrap ">
       {posts.map((item: card) => (
         <div
           key={item.id}
-          className="w-50 h-100 shadow-lg shadow-white rounded-lg"
+          className="w-50 h-100 border-1 border-white rounded-lg hover:shadow-lg hover:shadow-white hover:scale-105 transition-all ease-in-out duration-250 "
         >
           <Image
             src={item.image}
             alt="image"
-            width={0}
-            height={0}
-            sizes="100vw"
-            style={{ width: "100%", height: "200px" }}
+            width="500"
+            height="500"
+            className="w-full h-52 rounded-lg "
           />
           <div className="flex flex-col p-4">
             <h2>{item.id}</h2>
