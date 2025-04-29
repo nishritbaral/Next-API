67f03935baf41ef0ddb9e2fb383610e155f2d1dd
Nishrit Baral <nishritbaral@gmail.com>
2025-04-29 13:32:27 +0545

    passed the data as props fromm the page to the list

--- app/components/list.tsx
+++ app/components/list.tsx
@@ -1,35 +1,14 @@
 import React from "react";
 import Image from "next/image";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import Link from "next/link";
+import { Card } from "../page";
 
-type Card = {
-  id: number;
-  title: string;
-  price: number;
-  description: string;
-  category: string;
-  image: string;
-  rating: {
-    rate: number;
-    count: number;
-  };
-};
-
-const getData = async () => {
-  const data = await fetch("https://fakestoreapi.com/products", {
-    next: { revalidate: 600 },
-  });
-  return data.json();
-};
-
-const List = async () => {
-  // console.log(data);
-  const posts: Card[] = await getData();
-  // console.log(posts);
-
+const List = (props: { posts: Card[] }) => {
+  console.log(props.posts);
   return (
     <div className="flex flex-row items-center justify-center p-2 m-2 gap-8 max-w-lvw flex-wrap ">
-      {posts.map((item: Card) => (
+      {props.posts.map((item: Card) => (
         <div
           key={item.id}
           className=" flex flex-col w-58 h-96 border-1 border-white rounded-lg  opacity-80 hover:opacity-100 hover:shadow-lg hover:shadow-white hover:scale-105 transition-all ease-in-out duration-200"
@@ -45,9 +24,13 @@ const List = async () => {
             <h2>{item.id}</h2>
             <p>{item.title}</p>
 
-            <div className="flex justify-end items-end ">
+            <Link
+              href="https://www.youtube.com"
+              target="_blank"
+              className="flex justify-end items-end "
+            >
               <ExpandMoreIcon className=" w-32 h-32 border-2 border-white rounded-full bg-black text-white  hover:bg-white hover:text-black hover:scale-110 transition-all duration-300 ease-in-out" />
-            </div>
+            </Link>
           </div>
         </div>
       ))}
--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,33 @@ import React from "react";
 import Navbar from "./components/navbar";
 import List from "./components/list";
 
-const Home = () => {
+export type Card = {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+  rating: {
+    rate: number;
+    count: number;
+  };
+};
+
+export const getData = async () => {
+  const data = await fetch("https://fakestoreapi.com/products", {
+    next: { revalidate: 600 },
+  });
+  return data.json();
+};
+
+const Home = async () => {
+  const posts: Card[] = await getData();
+  console.log(posts);
   return (
     <div>
       <Navbar />
-      <List />
+      <List posts={posts} />
     </div>
   );
 };
